0a3ef24ba875423fc40d78c02a006a55554b19dc
gerson97 <g.isaias22@outlook.com>
2021-02-16 12:44:51 -0600

    Initial Commit

---
+++ StatusConnection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StatusConnection
+//
+//  Created by Gerson Isaias on 16/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
